f0245d80a6f7220d7f8e9313c4ff8e6d1bb0b9c9
Seyed Mustafa Moosavian <Parand8631@gmail.com>
2022-10-23 13:24:35 +0330

    design button for add_company, add_person, add_decree, add_wage in dashboard page

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,19 @@ module.exports = {
     ],
     theme: {
         extend: {
+            // colors: {
+            //     background: '#0f172a',
+            //     surface: '#1E293B',
+            //     // primary: '#9333ea',
+            //     primary: '#fcd535',
+            //     secondary: '#374151',
+            //     danger: '#e11d48',
+            //     onBackground: '#9ca3af',
+            //     onSurface: '#d1d5db',
+            //     onPrimary: '#111827',
+            //     onSecondary: '#F9FAFB',
+            //     onDanger: '#ffffff'
+            // },
             fontFamily: {
                 iransans: ['iransans'],
                 vazir: ['vazir'],
